4b77b8306d937748fbb9d7df71fef8ff965ed4d9
dberg <dberg@kabam.com>
2017-06-20 11:11:36 -0700

    Join oldest node when bootstrapping.

--- src/main/scala/kluster/Kluster.scala
+++ src/main/scala/kluster/Kluster.scala
@@ -4,23 +4,46 @@ import akka.actor.{ActorSystem, Address, Props}
 import akka.cluster.Cluster
 import akka.cluster.ClusterEvent.{MemberEvent, UnreachableMember}
 import akka.event.Logging
+import java.net.InetAddress
+import scala.annotation.tailrec
+import scala.util.Try
 
 object Kluster {
 
+  def isReachable(hostname: String): Boolean = Try[Boolean] {
+    InetAddress.getByName(hostname).isReachable(1000)
+  }.getOrElse(false)
+
+  @tailrec
+  def getRunningNode(system: ActorSystem, nodeNumber: Int = 1): Option[Address] = {
+    if (nodeNumber < 1 || nodeNumber > 1000) {
+      None
+    } else {
+      val nodeName = s"kluster$nodeNumber"
+      val reachable = isReachable(nodeName)
+      if (reachable) {
+        Some(Address("akka.tcp", system.name, nodeName, 2550))
+      } else {
+        getRunningNode(system, nodeNumber + 1)
+      }
+    }
+  }
+
+  // Create cluster programatically.
   def createCluster(implicit system: ActorSystem, hostname: String): Option[Cluster] = {
-    // Create cluster programatically.
-    val addr = Address("akka.tcp", system.name, "kluster1", 2550)
     if (hostname startsWith system.name) {
-      val cluster = Cluster(system)
-      // The first node joins itself and the remaining nodes join the first node.
-      cluster.join(addr)
-      // Subscribe to the cluster events.
-      cluster.subscribe(
-        system.actorOf(Props[KlusterObserver], s"Observer:$hostname"),
-        classOf[MemberEvent],
-        classOf[UnreachableMember]
-      )
-      Some(cluster)
+      getRunningNode(system) map { addr =>
+        val cluster = Cluster(system)
+        // The first node joins itself and the remaining nodes join the first node.
+        cluster.join(addr)
+        // Subscribe to the cluster events.
+        cluster.subscribe(
+          system.actorOf(Props[KlusterObserver], s"Observer:$hostname"),
+          classOf[MemberEvent],
+          classOf[UnreachableMember]
+        )
+        cluster
+      }
     } else {
       val log = Logging(system, "Kluster")
       log.error("Wrong hostname. No cluster will be created.")
--- src/main/scala/kluster/KlusterObserver.scala
+++ src/main/scala/kluster/KlusterObserver.scala
@@ -1,7 +1,7 @@
 package kluster
 
 import akka.actor.Actor
-import akka.cluster.ClusterEvent.{ CurrentClusterState, MemberExited, MemberJoined, MemberRemoved, MemberUp, ReachableMember, UnreachableMember, MemberWeaklyUp }
+import akka.cluster.ClusterEvent.{ CurrentClusterState, MemberExited, MemberJoined, MemberLeft, MemberRemoved, MemberUp, MemberWeaklyUp, ReachableMember, UnreachableMember }
 import akka.cluster.{ Member, MemberStatus }
 import akka.event.Logging
 
@@ -26,6 +26,8 @@ class KlusterObserver extends Actor {
       log.info(s"OBSERVER reachable member $member")
     case MemberWeaklyUp(member: Member) =>
       log.info(s"OBSERVER member weakly up $member")
+    case MemberLeft(member: Member) =>
+      log.info(s"OBSERVER member left $member")
     case msg =>
       log.info(s"OBSERVER got a weird message $msg")
   }
